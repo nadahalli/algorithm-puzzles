0d987d2bda12c88648c0cd1893283adb9d073e75
Tejaswi Nadahalli <nadahalli@gmail.com>
2013-11-01 11:17:20 +0530

    Fixed minmax bug

--- egg_dropping.py
+++ egg_dropping.py
@@ -13,14 +13,14 @@ def find_floor(neggs, nfloors):
     elif nfloors == 0:
         ret_val = 0
     else:
-        min_diff = float('inf')
+        minmax = float('inf')
         for floor in range(nfloors):
             pre = find_floor(neggs - 1, floor)
             post = find_floor(neggs, nfloors - floor - 1)
-            diff = pre - post if pre > post else post - pre
-            if diff < min_diff:
-                min_diff = diff
-                ret_val = pre + 1
+            min_canidate = max(pre, post)
+            if min_canidate < minmax:
+                minmax = min_canidate
+        ret_val = minmax + 1
         memo[memo_key] = ret_val
     return ret_val 
 
